a9a7cdaf36b5851b28450e84a1e6f242ea4a5a14
Ashley Radford <aradford7@outlook.com>
2019-05-26 12:47:03 -0700

    icons and descript on home

--- src/App.js
+++ src/App.js
@@ -7,14 +7,21 @@ import {Register} from './layout/Register/Register'
 import {Logout} from './layout/Logout'
 import {NoMatch} from './layout/NoMatch'
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
-//import {Switch, Route} from 'react-router-dom'
-
 
 //import * as routes from './constants/routes'
 ////////STYLING STUFF//////////
-
 import './App.css';
-
+import {library} from '@fortawesome/fontawesome-svg-core'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faHamburger} from '@fortawesome/free-solid-svg-icons'
+import {faHeart} from '@fortawesome/free-solid-svg-icons'
+import {faClock} from '@fortawesome/free-solid-svg-icons'
+import {faUsers} from '@fortawesome/free-solid-svg-icons'
+library.add(faHamburger)
+library.add(faHeart)
+library.add(faClock)
+library.add(faUsers)
+////////////////////////////////
 class App extends Component {
   state = {
 
