05c220962bf157b092406e03d7bcfe697c0139e6
Ashley Radford <aradford7@outlook.com>
2019-05-25 17:00:16 -0700

    carosel working not sized

--- src/layout/NavBar/NavBar.js
+++ src/layout/NavBar/NavBar.js
@@ -3,15 +3,14 @@ import {NavLink} from 'react-router-dom'
 
 import * as routes from '../../constants/routes'
 
-import './NavBar.css'
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
-
-
 import Button from 'react-bootstrap/Button'
+import ButtonToolbar from 'react-bootstrap/ButtonToolbar'
+import ButtonGroup from 'react-bootstrap/ButtonGroup'
 
 import logo from '../../logo.svg';
-
+import './NavBar.css'
 const NavBar = () =>
 <div>
 <Navbar bg="light" variant="light">
@@ -21,15 +20,15 @@ const NavBar = () =>
         width="30"
         height="30"
         className="d-inline-block align-top"
-      />
-      {'Our Site Title'}</Navbar.Brand>
+      /><span>{' MY THEMEPARK VISIT'}</span></Navbar.Brand>
     <Nav className="mr-auto">
         <NavLink exact to = {routes.HOME}><Nav.Link href="/"></Nav.Link></NavLink>
-        <NavLink to={routes.PARKS} activeClassName = "selected"> <Nav.Link href="/parks">Parks</Nav.Link></NavLink>
-        <NavLink to={routes.TODO} activeClassName = "selected"> <Nav.Link href="/activities">Things to Do</Nav.Link></NavLink>
-        <NavLink to={routes.REGISTER} activeClassName = "selected"><Button href = "/register">REGISTER</Button></NavLink>
-        <NavLink to={'/login'} activeClassName = "selected"><Button href = "/login">LOGIN</Button></NavLink>
- 
+    </Nav>
+    <Nav className="justify-content-end">
+        <NavLink  to={routes.PARKS} activeClassName = "selected"> <Nav.Link href="/parks">Parks</Nav.Link></NavLink>
+        <NavLink  to={routes.TODO} activeClassName = "selected"> <Nav.Link href="/activities">Things to Do</Nav.Link></NavLink>
+        <NavLink  to={routes.REGISTER} activeClassName = "selected"><ButtonToolbar><ButtonGroup  className="mr-2" aria-label="First group"><Button class ="btn" href = "/register">REGISTER</Button></ButtonGroup></ButtonToolbar></NavLink>
+        <NavLink  to={'/login'} activeClassName = "selected"><ButtonToolbar><ButtonGroup aria-label="Second group"><Button href = "/login">LOGIN</Button></ButtonGroup></ButtonToolbar></NavLink>
     </Nav>
   </Navbar>
      
