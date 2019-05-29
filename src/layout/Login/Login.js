00c085571022cf01120d9d809b040218131bdc9e
chaselancaster <chaseblancaster@gmail.com>
2019-05-29 15:06:30 -0700

    Add handleChange to Login component

--- src/layout/Login/Login.js
+++ src/layout/Login/Login.js
@@ -1,61 +1,88 @@
-import React from 'react'
-import NavBar from '../NavBar/NavBar'
-import Footer from '../Footer/Footer'
-import Parallax from '../Parallax/Parallax'
-import Form from 'react-bootstrap/Form'
-import Button from 'react-bootstrap/Button'
-import Alert from 'react-bootstrap/Alert'
+import React, { Component } from "react";
+import NavBar from "../NavBar/NavBar";
+import Footer from "../Footer/Footer";
+import Parallax from "../Parallax/Parallax";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import Alert from "react-bootstrap/Alert";
 
-import './Login.css'
+import "./Login.css";
 
-export default class Login extends React.Component {
-    render(){
-        
-        return(
-            <div className = "loginpg">
-                <NavBar/>
-                <Parallax/>
+class Login extends Component {
+  state = {
+    username: "",
+    password: ""
+  };
 
-                <div class = "container">
-                    <div class = "logincontainer">
-                <br/>
-                    <h2>Log in</h2>
-                    <h3>Use your email and password to log in.</h3>
-                </div>
+  handleChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
 
-                <div class = "logincontainer">
-                    <Form className = "formcontainer">
-                        <Form.Group controlId="formBasicUsername">
-                            <Form.Label>Username</Form.Label>
-                            <Form.Control type="username" placeholder="Enter Username" />
-                            <Form.Text className="text-muted">
-                                Please enter your Username or Email.
-                            </Form.Text>
-                        </Form.Group>
+  render() {
+    return (
+      <div className="loginpg">
+        <NavBar />
+        <Parallax />
 
-                        <Form.Group controlId="formBasicPassword">
-                            <Form.Label>Password</Form.Label>
-                            <Form.Control type="password" placeholder="Password" />
-                        </Form.Group>
+        <div class="container">
+          <div class="logincontainer">
+            <br />
+            <h2>Log in</h2>
+            <h3>Use your email and password to log in.</h3>
+          </div>
 
-                        <Form.Group controlId="formBasicChecbox">
-                            <Form.Check type="checkbox" label="Save Password" />
-                        </Form.Group>
+          <div class="logincontainer">
+            <Form className="formcontainer">
+              <Form.Group controlId="formBasicUsername">
+                <Form.Label>Username</Form.Label>
+                <Form.Control
+                  name="username"
+                  type="username"
+                  placeholder="Enter Username"
+                  onChange={this.handleChange}
+                />
+                <Form.Text className="text-muted">
+                  Please enter your Username or Email.
+                </Form.Text>
+              </Form.Group>
 
-                        <Alert variant="light">
-                            <Alert.Link href="/register">Don't have an account yet? Register now. </Alert.Link>
-                        </Alert>
+              <Form.Group controlId="formBasicPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control
+                  name="password"
+                  type="password"
+                  placeholder="Password"
+                  onChange={this.handleChange}
+                />
+              </Form.Group>
 
-                        <Button variant="primary" type="submit" href ="/itinerary">Sign In</Button>
-                    </Form>
-                </div>
-            </div>
+              <Form.Group controlId="formBasicChecbox">
+                <Form.Check type="checkbox" label="Save Password" />
+              </Form.Group>
+
+              <Alert variant="light">
+                <Alert.Link href="/register">
+                  Don't have an account yet? Register now.{" "}
+                </Alert.Link>
+              </Alert>
+
+              <Button variant="primary" type="submit" href="/itinerary">
+                Sign In
+              </Button>
+            </Form>
+          </div>
+        </div>
 
         <div>
-            <br/><br/>
-            <Footer/>
+          <br />
+          <br />
+          <Footer />
         </div>
-</div>
-        )
-    }
+      </div>
+    );
+  }
 }
+
+export default Login;
