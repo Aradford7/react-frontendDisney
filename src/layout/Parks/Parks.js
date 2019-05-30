15020d86b12000b4718f92907e50516b8341cafd
Ashley Radford <aradford7@outlook.com>
2019-05-30 11:22:15 -0700

    add park page route

--- src/layout/Parks/Parks.js
+++ src/layout/Parks/Parks.js
@@ -2,11 +2,47 @@ import React from 'react'
 
 import NavBar from '../NavBar/NavBar'
 import Footer from '../Footer/Footer'
+import {Row, Col, Card, Image} from 'react-bootstrap'
 
 export const Parks = () =>(
     <div>
         <NavBar/>
         <h2>Parks PAGE </h2>
+        <div class = "pics">
+                <Row lg = {true}>
+                     <Col md={4} aria-label="first group">
+                     <Card style={{ width: '25rem' }}>
+                         <Image  
+                            width = "400px"
+                            height = "300px"
+                            src="https://cdn.ventrata.com/image/upload/ar_1.5,c_fill,dpr_3.0,f_jpg,w_400/v1506370830/vimwsuqqtctakrxba5r6.jpg" rounded/>
+
+                                <Card.Body><a href = "/disneyland">Disneyland - Anaheim, CA</a></Card.Body>
+                            </Card>
+                    </Col>
+
+                    <Col md={4} aria-label="second group">
+                    <Card style={{ width: '25rem' }}>
+                        <Image 
+                            width = "400px"
+                            height = "300px"
+                            src="https://www.discoverlosangeles.com/sites/default/files/styles/hero/public/images/2019-01/USH_Tour_Yuri%201.jpg" rounded  />
+                                <Card.Body><a href = "/ush">Universal Studios Hollywood -Universal City, CA</a></Card.Body>
+                            </Card>
+                    </Col>
+
+                    <Col md= {4} aria-label="third group">
+                    <Card  style={{ width: '25rem' }}> 
+                        <Image  
+                            width = "400px"
+                            height = "300px"
+                            src="https://secure.cdn1.wdpromedia.com/resize/mwImage/1/640/360/75/dam/wdpro-assets/dlr/gallery/destinations/california-adventure-park/disney-california-adventure-gallery00.jpg" rounded />
+                                <Card.Body><a href = "/caladventures">Disney California Adventure - Anaheim, CA</a></Card.Body>
+                        </Card>
+                    </Col>
+                </Row>
+       
+        </div>
     
         <Footer/>
     </div>
---
+++ src/layout/ush/ush.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Ush= () =>(
+    <div>
+        <h2>USH Rides</h2>
+        <p>list of rides</p>
+        <h2>USH Restruants</h2>
+
+    </div>
+)
