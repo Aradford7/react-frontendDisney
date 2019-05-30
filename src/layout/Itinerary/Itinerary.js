cac95484f8febd2731681f442d85fb9d27e33645
chaselancaster <chaseblancaster@gmail.com>
2019-05-29 18:52:49 -0700

    Add handleChange to Itinerary.js

--- src/layout/Itinerary/Itinerary.js
+++ src/layout/Itinerary/Itinerary.js
@@ -1,13 +1,12 @@
-import React, { Component } from 'react';
-import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
-import NavBar from '../NavBar/NavBar'
-import Footer from '../Footer/Footer'
-import Form from 'react-bootstrap/Form'
-import Calendar from '../Calendar/Calender'
-import { Container, Row, Col} from 'react-bootstrap';
+import React, { Component } from "react";
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from "reactstrap";
+import NavBar from "../NavBar/NavBar";
+import Footer from "../Footer/Footer";
+import Form from "react-bootstrap/Form";
+import Calendar from "../Calendar/Calender";
+import { Container, Row, Col } from "react-bootstrap";
 
-import './Itinerary.css'
-
+import "./Itinerary.css";
 
 class Itinerary extends Component {
   constructor(props) {
@@ -15,6 +14,8 @@ class Itinerary extends Component {
     this.state = {
       modal: false,
       lgShow: false,
+      name: "",
+      park: ""
     };
 
     this.toggle = this.toggle.bind(this);
@@ -26,95 +27,135 @@ class Itinerary extends Component {
     }));
   }
 
+  handleChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
+
   render() {
     const lgClose = () => this.setState({ lgShow: false });
-    const externalCloseBtn = <button className="close" style={{ position: 'absolute', top: '15px', right: '15px' }} onClick={this.toggle}>&times;</button>;
+    const externalCloseBtn = (
+      <button
+        className="close"
+        style={{ position: "absolute", top: "15px", right: "15px" }}
+        onClick={this.toggle}
+      >
+        &times;
+      </button>
+    );
     return (
       <div>
-
         <div>
-           <NavBar/>
+          <NavBar />
         </div>
 
         <h2>My Itineraries</h2>
         <p>Welcome! You have no itineraries yet</p>
-        <p> This should be the Itinarary show page for an Individual User after they logged in</p>
+        <p>
+          {" "}
+          This should be the Itinarary show page for an Individual User after
+          they logged in
+        </p>
 
-        <div className = "Modal">
+        <div className="Modal">
           <h1> Create A New Itinerary</h1>
-        <div className = "formModal">
-        <Button color="danger" onClick={this.toggle}>{this.props.buttonLabel}Add A New Visit</Button>
-        <Modal isOpen={this.state.modal} toggle={this.toggle} className={this.props.className} external={externalCloseBtn} size="lg" show={this.state.lgShow} onHide={lgClose}>
-          <ModalHeader>Add new itinerary</ModalHeader>
-          <ModalBody>
+          <div className="formModal">
+            <Button color="danger" onClick={this.toggle}>
+              {this.props.buttonLabel}Add A New Visit
+            </Button>
+            <Modal
+              isOpen={this.state.modal}
+              toggle={this.toggle}
+              className={this.props.className}
+              external={externalCloseBtn}
+              size="lg"
+              show={this.state.lgShow}
+              onHide={lgClose}
+            >
+              <ModalHeader>Add new itinerary</ModalHeader>
+              <ModalBody>
+                <Form>
+                  <Form.Row>
+                    <Form.Group as={Col} controlId="formGridEmail">
+                      <Form.Label>Visit Name</Form.Label>
+                      <Form.Control
+                        name="name"
+                        type="text"
+                        placeholder="Visit name is required."
+                      />
+                    </Form.Group>
+                  </Form.Row>
 
+                  <Form.Group as={Col} controlId="formGridPark">
+                    <Form.Label>Which theme park are you attending?</Form.Label>
+                    <Form.Control as="select" name="park">
+                      <option>Choose...</option>
+                      <option>Disneyland - Anaheim, California</option>
+                      <option>
+                        Disney Land California Adventures - Anaheim, California
+                      </option>
+                      <option>
+                        Universal Studios Hollywood - Studios, City
+                      </option>
+                    </Form.Control>
+                  </Form.Group>
 
-           <Form>
-            <Form.Row>
-              <Form.Group as={Col} controlId="formGridEmail">
-                  <Form.Label>Visit Name</Form.Label>
-                  <Form.Control type="email" placeholder="Visit name is required." />
-              </Form.Group>
-            </Form.Row>
+                  <Form.Row>
+                    <Form.Group as={Col} xs={6} controlId="formGridDate">
+                      <Form.Label>Which date will ?</Form.Label>
+                      <Calendar />
+                    </Form.Group>
 
-
-            <Form.Group as={Col} controlId="formGridPark">
-                  <Form.Label>Which theme park are you attending?</Form.Label>
-                  <Form.Control as="select">
-                        <option>Choose...</option>
-                        <option>Disneyland - Anaheim, California</option>
-                        <option>Disney Land California Adventures - Anaheim, California</option>
-                        <option>Universal Studios Hollywood -  Studios, City</option>
-                  </Form.Control>
-              </Form.Group>
-
-            <Form.Row>
-              <Form.Group as={Col}  xs = {6} controlId="formGridDate">
-                  <Form.Label>Which date will ?</Form.Label>
-                  <Calendar/>
-              </Form.Group>
-              
-              <Form.Group as={Col} xs = {6} controlId="formGridDate">
-                  <Form.Label>Check-out Date</Form.Label>
-                  <Calendar/>
-              </Form.Group>
-          </Form.Row>
-      </Form>
-
-
-
-          </ModalBody>
-          <ModalFooter>
-            <Button color="primary" onClick={this.toggle} href = "/newitinerary">Do Something</Button>{' '}
-            <Button color="secondary" onClick={this.toggle}>Cancel</Button>
-          </ModalFooter>
-        </Modal>
+                    <Form.Group as={Col} xs={6} controlId="formGridDate">
+                      <Form.Label>Check-out Date</Form.Label>
+                      <Calendar />
+                    </Form.Group>
+                  </Form.Row>
+                </Form>
+              </ModalBody>
+              <ModalFooter>
+                <Button
+                  color="primary"
+                  onClick={this.toggle}
+                  href="/newitinerary"
+                >
+                  Do Something
+                </Button>{" "}
+                <Button color="secondary" onClick={this.toggle}>
+                  Cancel
+                </Button>
+              </ModalFooter>
+            </Modal>
+          </div>
         </div>
-     </div>
 
         <div>
-        <p>if there was any items already in user array show on this page, this will need to render a component that list previous itineraries and able to delete overrall array maybe</p>
-        <Button>Delete</Button>
+          <p>
+            if there was any items already in user array show on this page, this
+            will need to render a component that list previous itineraries and
+            able to delete overrall array maybe
+          </p>
+          <Button>Delete</Button>
         </div>
-        
-        <div class = "formhero">
-        <Container>
+
+        <div class="formhero">
+          <Container>
             <Row>
               <Col lg={true}>
-              <Container>
-                <Row>
-                    <Col md={6} aria-label="first group"></Col>
-                    <Col md={6} aria-label="second group"></Col>
-                </Row>
-            </Container>
+                <Container>
+                  <Row>
+                    <Col md={6} aria-label="first group" />
+                    <Col md={6} aria-label="second group" />
+                  </Row>
+                </Container>
               </Col>
             </Row>
-        </Container>
+          </Container>
         </div>
 
-
         <div>
-          <Footer/>
+          <Footer />
         </div>
       </div>
     );
